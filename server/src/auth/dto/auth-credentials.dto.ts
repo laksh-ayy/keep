fd539004bae4146ec73520c7e8d02d91401ebeae
Lakshay Khurana <laksh@Lakshays-MacBook-Air.local>
2021-12-08 17:11:53 +0530

    server complete

---
+++ server/src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, MinLength, MaxLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  password: string;
+}
