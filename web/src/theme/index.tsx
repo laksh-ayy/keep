bfdb9c96d086bf1a00d6ad524295a6ea71953196
Lakshay Khurana <laksh@Lakshays-MacBook-Air.local>
2021-12-09 18:07:00 +0530

    extra file removed

--- web/src/theme/index.tsx
+++ web/src/theme/index.tsx
@@ -1,14 +1,10 @@
 import { ThemeProvider } from "@mui/material";
-import { GlobalCss } from "./defaultCss";
 import { theme } from "./theme";
 
 export const MuiThemeProvider: React.FC = ({ children }) => {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalCss />
-        {children}
-      </ThemeProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </>
   );
 };
